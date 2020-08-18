f21489b7cd3d5d47ea5c781bdd138dfa084586a6
zHigor33 <higor_pinho@estudante.sc.senai.br>
2020-08-18 19:06:07 -0300

    L2E1

--- Estrutura de Decisão/L2E1.py
+++ Estrutura de Decisão/L2E1.py
@@ -1,1 +1,8 @@
-a
+firstNumber = float(input("Digite o 1° número: "))
+secondNumber = float(input("Digite o 2° número: "))
+
+if(firstNumber > secondNumber):
+    print("O maior dos número é o",firstNumber)
+else:
+    print("O maior dos número é o",secondNumber)
+    
