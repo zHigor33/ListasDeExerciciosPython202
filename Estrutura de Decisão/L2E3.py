dc92291f0b1b06e553a1406ba2da3624385d4764
zHigor33 <higor_pinho@estudante.sc.senai.br>
2020-08-18 19:41:56 -0300

    Exercicio 3

---
+++ Estrutura de Decisão/L2E3.py
@@ -0,0 +1,10 @@
+sex = str(input("Informe seu sexo(F/m): "))
+
+if(sex.lower() == "f"):
+    print("\nVocê é do sexo feminino")
+
+elif(sex.lower() == "m"):
+    print("\nVocê é do sexo masculino")
+
+else:
+    print("\nOpição invalida, tente de novo!")
