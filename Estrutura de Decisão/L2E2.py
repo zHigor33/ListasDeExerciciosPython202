cb82b162cb1e58d8df73d2ff75a2ec8055030c3f
zHigor33 <higor_pinho@estudante.sc.senai.br>
2020-08-18 19:35:57 -0300

    Exercicio 2

--- Estrutura de Decisão/L2E2.py
+++ Estrutura de Decisão/L2E2.py
@@ -1,1 +1,6 @@
-print("teste3")
+positiveNegative = float(input("Informe um valor: "))
+
+if(positiveNegative > 0):
+    print("O valor %d é positivo" % (positiveNegative))
+else:
+    print("O valor %d é negativo" % (positiveNegative))
